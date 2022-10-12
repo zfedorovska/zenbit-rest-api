e0daaa6de45132ef916aecba8bfc85eaa55a817c
Zoryana <zfedorovska@gmail.com>
2022-10-13 01:05:46 +0300

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { errorHandler } = require('./helpers/apiHelpers');
 
 app.use(errorHandler);
 
-const PORT = process.env.port;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });

f611693a2f2f6d8f8d9b0968072801e0c7e78523
Zoryana <zfedorovska@gmail.com>
2022-10-13 10:29:38 +0300

    modified db connection

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,22 @@
-const db = require('knex')({
+// const db = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     host: '127.0.0.1',
+//     port: 5432,
+//     user: 'postgres',
+//     password: 'admin',
+//     database: 'feedback',
+//   },
+// });
+
+const knex = require('knex');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'postgres',
-    password: 'admin',
-    database: 'feedback',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
